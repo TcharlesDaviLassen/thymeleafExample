5455b4d51038477b8c6b2fbd0f90bf3d3dd9d82e
TcharlesDaviLassen <tcharlesdavilassen@gmail.com>
2023-05-30 15:22:17 -0300

    Reajuste de data

--- src/main/java/com/refactorTest/test/controller/MeuController.java
+++ src/main/java/com/refactorTest/test/controller/MeuController.java
@@ -8,12 +8,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.Console;
+import java.text.DateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Controller
 public class MeuController {
+
+    private LocalDate dataDeInscricao;
 
     @Autowired
     private ServletContext servletContext;
@@ -27,10 +34,17 @@ public class MeuController {
         //
         model.addAttribute("itens", Arrays.asList("item 1", "item 2", "item 3"));
         //
-        model.addAttribute("diaSemana", "*");
+        Date data = new Date();
+        DateFormat dff = DateFormat.getDateInstance(DateFormat.FULL);
+        String dia = String.valueOf(dff.format(data).split(",")[0]);
+        System.out.println(dia);
+        model.addAttribute("diaSemana", dia);
         //
         boolean condicao = true;
         model.addAttribute("condicao", condicao);
+
+        boolean condicaoUn = true;
+        model.addAttribute("condicaoUn", condicaoUn);
         //
         List<String> listaDeItens = Arrays.asList("Item 1", "Item 2", "Item 3");
         model.addAttribute("listaDeItens", listaDeItens);
@@ -47,14 +61,14 @@ public class MeuController {
 
         //
         List<Produto> produtos = new ArrayList<>();
-        produtos.add(new Produto(2L,"Produto 1"));
-        produtos.add(new Produto(3L,"Produto 2"));
+        produtos.add(new Produto(2L, "Produto 1"));
+        produtos.add(new Produto(3L, "Produto 2"));
         model.addAttribute("produtos", produtos);
 
-        //        String path = "src/main/resources/assets/";
-        //        String imageName = "Captura.png";
-        //        model.addAttribute("path", path);
-        //        model.addAttribute("imageName", imageName);
+        // String path = "src/main/resources/assets/";
+        // String imageName = "Captura.png";
+        // model.addAttribute("path", path);
+        // model.addAttribute("imageName", imageName);
 
         String assetsPath = servletContext.getRealPath("/static");
         model.addAttribute("assetsPath", assetsPath);
